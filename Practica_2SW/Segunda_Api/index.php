f39e957b2f49636b4460e3fb7b0bb3ba451ddf8b
sandrocardona <sandrocardona41@gmail.com>
2024-05-08 14:31:33 +0200

    8 mayo

--- Practica_2SW/Segunda_Api/index.php
+++ Practica_2SW/Segunda_Api/index.php
@@ -1,31 +1,85 @@
 <?php
-    require __DIR__ . '/Slim/autoload.php';
-    require "./src/constantes.php";
+require __DIR__ . '/Slim/autoload.php';
+require "./src/constantes.php";
 
-    $app = new \Slim\App;
+$app = new \Slim\App;
 
 
-    //insertar usuario
-    $app->post("/insertar_usuario", function($request){
+//login
+$app->post("/login", function($request){
 
-        $nombre =$request->getParamm("nombre");
-        $usuario =$request->getParamm("usuario");
-        $clave =$request->getParamm("clave");
-        $dni =$request->getParamm("dni");
-        $sexo =$request->getParamm("sexo");
-        $suscripcion = $request->getParamm("suscripcion");
+    $datos[] = $request->getParamm("usuario");
+    $datos[] = $request->getParamm("clave");
 
-        echo json_encode(insertar_usuario($nombre, $usuario, $clave, $dni, $sexo, $suscripcion));
-    });
+    echo json_encode(login($datos));
+});
 
-    //Actualizar foto
-    $app->put("/actualizar_foto/{id_usuario}", function ($request) {
+//logueado
+$app->post("/logueado", function($request){
 
-        $id_usuario = $request->getAttribute("id_usuario");
-        $nombre_foto = $request->getParamm("nombre_foto");
+    session_id($request->getParamm("api_key"));
+    session_start();
 
-        echo json_encode(actualizar_foto($id_usuario, $nombre_foto));
-    });
+    if(isset($_SESSION["usuario"])){
 
+        $datos[] = $_SESSION["usuario"];
+        $datos[] = $_SESSION["clave"];
+
+        echo json_encode(logueado($datos));
+    } else {
+        session_destroy();
+        $respuesta["no_auth"] = "No tienes permiso para usar este servicio";
+        echo json_encode($respuesta);
+    }
+});
+
+//salir
+$app->post("/salir", function($request){
+    session_id($request->getParamm("api_key"));
+    session_start();
+    session_destroy();
+
+    $respuesta["logout"] = "Sesión cerrada";
+
+    echo json_encode($respuesta);
+});
+
+
+//insertar usuario
+$app->post("/insertar_usuario", function ($request) {
+
+    $nombre = $request->getParamm("nombre");
+    $usuario = $request->getParamm("usuario");
+    $clave = $request->getParamm("clave");
+    $dni = $request->getParamm("dni");
+    $sexo = $request->getParamm("sexo");
+    $suscripcion = $request->getParamm("suscripcion");
+
+    echo json_encode(insertar_usuario($nombre, $usuario, $clave, $dni, $sexo, $suscripcion));
+});
+
+//Actualizar foto
+$app->put("/actualizar_foto/{id_usuario}", function ($request) {
+
+    $id_usuario = $request->getAttribute("id_usuario");
+    $nombre_foto = $request->getParamm("nombre_foto");
+
+    echo json_encode(actualizar_foto($id_usuario, $nombre_foto));
+});
+
+//actualizar usuario
+$app->post("/actualizar_usuario_clave/{id_usuario}", function ($request) {
+
+    $nombre = $request->getParamm("nombre");
+    $usuario = $request->getParamm("usuario");
+    $clave = $request->getParamm("clave");
+    $dni = $request->getParamm("dni");
+    $sexo = $request->getParamm("sexo");
+    $suscripcion = $request->getParamm("suscripcion");
+
+    $id_usuario = $request->getAttribute("id_usuario");
+
+    echo json_encode(insertar_usuario($nombre, $usuario, $clave, $dni, $sexo, $suscripcion, $id_usuario));
+});
 
 $app->run();
--- Practica_2SW/Segunda_Api/src/constantes.php
+++ Practica_2SW/Segunda_Api/src/constantes.php
@@ -12,6 +12,86 @@
 
     /* ===== FUNCIONES ===== */
 
+    //login
+    function login($datos){
+        try{
+            $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
+        }
+    
+        catch(PDOException $e){
+            $respuesta["error"]="Imposible conectar a la BD en login(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+    
+        try{
+            $consulta = "SELECT * FROM usuarios WHERE usuario=? AND clave=?";
+            $sentencia=$conexion->prepare($consulta);
+            $sentencia->execute($datos);
+        }
+        
+        catch(PDOException $e){
+            $conexion=null;
+            $sentencia=null;
+            $respuesta["error"]="Imposible realizar la consulta en login(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+
+        if($sentencia->rowCount() > 0){
+
+            session_name("API_Pract2_Rec_23_24");
+            session_start();
+
+            $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+
+            $_SESSION["usuario"] = $respuesta["usuario"]["usuario"];
+            $_SESSION["clave"] = $respuesta["usuario"]["clave"];
+            $_SESSION["tipo"] = $respuesta["usuario"]["tipo"];
+
+            $respuesta["api_key"] = session_id();
+        } else {
+            $respuesta["mensaje"] = "Usuario no registrado en la BD";
+        }
+
+        $sentencia=null;
+        $conexion=null;
+        
+        return $respuesta;
+    }
+
+    //logueado
+    function logueado($datos){
+        try{
+            $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
+        }
+    
+        catch(PDOException $e){
+            $respuesta["error"]="Imposible conectar a la BD en login(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+    
+        try{
+            $consulta = "SELECT * FROM usuarios WHERE usuario=? AND clave=?";
+            $sentencia=$conexion->prepare($consulta);
+            $sentencia->execute($datos);
+        }
+        
+        catch(PDOException $e){
+            $conexion=null;
+            $sentencia=null;
+            $respuesta["error"]="Imposible realizar la consulta en login(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+
+        if($sentencia->rowCount() > 0)
+            $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+        else 
+            $respuesta["mensaje"] = "Usuario no registrado en la BD";
+
+        $sentencia=null;
+        $conexion=null;
+        
+        return $respuesta;
+    }
 
     //insertar usuario
 
@@ -71,6 +151,39 @@
             $conexion=null;
             $sentencia=null;
             $respuesta["error"]="Imposible realizar la consulta en actualizar_foto(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+
+        $sentencia=null;
+        $conexion=null;
+    }
+
+    // actualizar usuario
+
+    function actualizar_usuario_clave($nombre, $usuario, $clave, $dni, $sexo, $suscripcion, $id_usuario){
+        try{
+            $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
+        }
+    
+        catch(PDOException $e){
+            $respuesta["error"]="Imposible conectar a la BD en insertar_usuario(). Error:".$e->getMessage();
+            return $respuesta;
+        }
+    
+        try{
+            $consulta = "UPDATE INTO usuarios set nombre=?, usuario=?, dni=?, sexo=?, suscripcion=?, WHERE id_usuario=?";
+            $sentencia=$conexion->prepare($consulta);
+            $sentencia->execute([$nombre, $usuario, $clave, $dni, $sexo, $suscripcion, $id_usuario]);
+
+            $respuesta["ultm_id"] = $conexion->lastInsertId();
+
+            return $respuesta;
+        }
+        
+        catch(PDOException $e){
+            $conexion=null;
+            $sentencia=null;
+            $respuesta["error"]="Imposible realizar la consulta en instertar_usuario(). Error:".$e->getMessage();
             return $respuesta;
         }
 
