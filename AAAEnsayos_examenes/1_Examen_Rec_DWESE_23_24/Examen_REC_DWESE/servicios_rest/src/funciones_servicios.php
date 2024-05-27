6981b93b98b717e7d5277c599110c5890c4761e9
sandrocardona <sandrocardona41@gmail.com>
2024-05-27 08:30:26 +0200

    11

---
+++ AAAEnsayos_examenes/1_Examen_Rec_DWESE_23_24/Examen_REC_DWESE/servicios_rest/src/funciones_servicios.php
@@ -0,0 +1,52 @@
+<?php
+require "config_bd.php";
+
+
+
+function login($usuario,$clave)
+{
+    try{
+        $conexion= new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'"));
+        
+    }
+    catch(PDOException $e){
+        $respuesta["error"]="Imposible conectar:".$e->getMessage();
+        return $respuesta;
+    }
+
+    try{
+        $consulta="select * from usuarios where usuario=? and clave=?";
+        $sentencia=$conexion->prepare($consulta);
+        $sentencia->execute([$usuario,$clave]);
+
+    }
+    catch(PDOException $e){
+
+        $respuesta["error"]="Imposible conectar:".$e->getMessage();
+        $sentencia=null;
+        $conexion=null;
+        return $respuesta;
+    }
+
+    if($sentencia->rowCount()>0)
+    {
+        $respuesta["usuario"]=$sentencia->fetch(PDO::FETCH_ASSOC);
+        session_name("API_ExamRec_23_24");
+        session_start();
+        $_SESSION["usuario"]=$respuesta["usuario"]["usuario"];
+        $_SESSION["clave"]=$respuesta["usuario"]["clave"];
+        $_SESSION["tipo"]=$respuesta["usuario"]["tipo"];
+        $respuesta["api_session"]=session_id();
+    }
+    else
+    {
+        $respuesta["mensaje"]="Usuario no se encuentra regis. en la BD";
+    }
+
+    $sentencia=null;
+    $conexion=null;
+    return $respuesta;
+}
+
+
+?>
